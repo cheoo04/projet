8cd23b767c7e1c81d0db96d8ca913ff0a6a65210
cheoo04 <yah.kouakou24@inphb.ci>
2026-06-17 10:54:14 +0000

    chore: update Firebase SDK, add cloud_functions, fix imports

--- lib/screens/modern_product_detail_screen.dart
+++ lib/screens/modern_product_detail_screen.dart
@@ -1,3 +1,5 @@
+import '../services/product_service.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
